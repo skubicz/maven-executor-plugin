d63fdc1332fa3295603280a42718fef43e990ba3
Szymon Kubicz <k-szymek@wp.pl>
2018-12-02 00:37:11 +0100

    Revert version 1.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "org.kubicz.mavenexecutor"
-version = "1.0.2-SNAPSHOT"
+version = "1.0.1"
 
 buildscript {
     repositories { mavenCentral() }
