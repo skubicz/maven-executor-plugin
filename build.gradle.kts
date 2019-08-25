3bf1722375757816448b102b49132b3016a88727
Szymon Kubicz <k-szymek@wp.pl>
2019-08-25 19:37:09 +0200

    Set version 1.0.5-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "org.kubicz.mavenexecutor"
-version = "1.0.4"
+version = "1.0.5-SNAPSHOT"
 
 buildscript {
     repositories { mavenCentral() }
