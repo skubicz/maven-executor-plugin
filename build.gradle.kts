b4e9abda1ff27aa9a0680920b82a77b3c222d831
Szymon <kubicz.szymon@gmail.com>
2021-02-05 20:37:31 +0100

    Set version 2.0.1-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "org.kubicz.mavenexecutor"
-version = "2.0.0"
+version = "2.0.1-SNAPSHOT"
 
 buildscript {
     repositories { mavenCentral() }
